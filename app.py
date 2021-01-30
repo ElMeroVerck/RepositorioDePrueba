2a0635efd4c456cc5d243aa5b3a3a38de187fc40
ElMeroVerck <erickemanuelolmedo@gmail.com>
2021-01-29 20:44:16 -0600

    Update2

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return "Hola a todos"
+    return render_template("index.html")
 
-if __name__ == "_main_":
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
+    
